45f762907658f01724bc5534935b728e8313c78b
Ian-Remi Lafleur <irlafleur@gmail.com>
2015-09-01 14:26:40 -0400

    Created person scheme for person integration

---
+++ src/main/java/family/dao/PersonDAO.java
@@ -0,0 +1,13 @@
+package family.dao;
+
+import java.util.List;
+
+import com.journaldev.model.Person;
+ 
+public interface PersonDAO {
+ 
+    public void save(Person p);
+     
+    public List<Person> list();
+     
+}
