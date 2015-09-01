4cdbfc8343e7f081f4f7c752b5e97953f91af117
Ian-Remi Lafleur <irlafleur@gmail.com>
2015-09-01 13:52:09 -0400

    first commit

---
+++ src/main/java/family/MainController.java
@@ -0,0 +1,18 @@
+package family;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public FamilyTasks familyTasks(@RequestParam(value="name", defaultValue="World") String name) {
+        return new FamilyTasks(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
---
+++ src/main/java/family/MainInterface.java
@@ -0,0 +1,13 @@
+package family;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainInterface {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainInterface.class, args);
+    }
+}
