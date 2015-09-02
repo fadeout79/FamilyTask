036e3e0a1cb161b1ffc8026f9d905b068c2985a7
Ian-Remi Lafleur <irlafleur@gmail.com>
2015-09-01 20:06:40 -0400

    Corrected SQL script

--- setup.sql
+++ setup.sql
@@ -1,63 +1,66 @@
-CREATE TABLE 'person' (
-	`id` INT (10) NULL DEFAULT NULL,
-	`name` VARCHAR(50) NULL DEFAULT NULL,
-	`dateOfBirth` TIMESTAMP NULL DEFAULT NULL
-)
+DROP TABLE IF EXISTS person;
+DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS taskPoints;
+DROP TABLE IF EXISTS rewards;
+DROP TABLE IF EXISTS spentRewards;
+DROP VIEW IF EXISTS accumulatedPoints;
+DROP VIEW IF EXISTS spentPoints;
 
-CREATE TABLE 'tasks' (
-	`id` INT (10) NULL DEFAULT NULL,
-	`personId` INT (10) NULL DEFAULT NULL,
-	`summary` VARCHAR(50) NULL DEFAULT NULL,
-	`description` VARCHAR(255) NULL DEFAULT NULL,
-	`isRedundant` BOOLEAN NULL DEFAULT FALSE,
-	`points` INT (10) NULL DEFAULT NULL
-)
+CREATE TABLE person (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(50) NOT NULL,
+	dateOfBirth DATE,
+	isParent BOOLEAN DEFAULT FALSE,
+	PRIMARY KEY (id)
+);
 
-CREATE TABLE 'taskPoints' (
-	`id` INT (10) NULL DEFAULT NULL,
-	`personId` INT (10) NULL DEFAULT NULL,
-	`tasksId` INT (10) NULL DEFAULT NULL,
-	`multiplicationFactor` DOUBLE NULL DEFAULT NULL,
-	`dateEarned` TIMESTAMP NULL DEFAULT NULL 
-)
+CREATE TABLE tasks (
+	id INT NOT NULL AUTO_INCREMENT,
+	personId INT NULL DEFAULT NULL,
+	summary VARCHAR(50) NULL DEFAULT NULL,
+	description VARCHAR(255) NULL DEFAULT NULL,
+	isRedundant BOOLEAN NULL DEFAULT FALSE,
+	points INT NULL DEFAULT NULL,
+	PRIMARY KEY (id)
+);
 
-CREATE TABLE 'rewards' (
-	`id` INT (10) NULL DEFAULT NULL,
-	`summary` VARCHAR(50) NULL DEFAULT NULL,
-	`description` VARCHAR(255) NULL DEFAULT NULL,
-	`points` INT (10) NULL DEFAULT NULL
-)
+CREATE TABLE taskPoints (
+	id INT NOT NULL AUTO_INCREMENT,
+	personId INT NOT NULL,
+	tasksId INT NOT NULL,
+	multiplicationFactor DOUBLE NOT NULL,
+	dateEarned TIMESTAMP NOT NULL,
+	PRIMARY KEY (id)
+);
 
-CREATE TABLE 'spentRewards' (	
-	`id` INT (10) NULL DEFAULT NULL,
-	`personId` INT (10) NULL DEFAULT NULL,
-	`rewardsId` INT (10) NULL DEFAULT NULL
-)
+CREATE TABLE rewards (
+	id INT NOT NULL AUTO_INCREMENT,
+	summary VARCHAR(50) NOT NULL,
+	description VARCHAR(255) NOT NULL,
+	points INT (10) NULL,
+	PRIMARY KEY (id)
+);
 
+CREATE TABLE spentRewards (	
+	id INT NOT NULL AUTO_INCREMENT,
+	personId INT (10) NULL,
+	rewardsId INT (10) NULL,
+	PRIMARY KEY (id)
+);
 
-DELIMITER //
-CREATE PROCEDURE availablePoints ( IN personId INT, 
-								   OUT availablePoints INT
-								   )
-BEGIN
-	DECLARE pointsWon INT;
-	DECLARE pointsSpent INT;
-	
-	SELECT Sum(points) 
+CREATE VIEW accumulatedPoints AS
+	SELECT Sum(points), taskPoints.personId
 	FROM tasks 
 	INNER JOIN taskPoints
-	WHERE tasks.id = taskPoints.tasksId AND
-	      taskPoints.personId = personId
-	INTO pointsWon;
+	WHERE tasks.id = taskPoints.tasksId
+	GROUP BY taskPoints.personId;
 	
-	SELECT Sum(points) 
+CREATE VIEW spentPoints AS
+	SELECT Sum(points), spentRewards.personId 
 	FROM rewards 
 	INNER JOIN spentRewards
-	WHERE rewards.id = spentRewards.rewardsId AND
-	      spentRewards.personId = personId
-	INTO pointsSpent;
-	
-	avalablePoints = pointsWon - pointsSpent;
-END //
-DELIMITER ;
+	WHERE rewards.id = spentRewards.rewardsId
+	GROUP BY spentRewards.personId;
+	      
+
 
