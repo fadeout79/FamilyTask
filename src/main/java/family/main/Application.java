999d5125f2ed8f55d9869424a644ef766ca04c29
Ian-Remi Lafleur <irlafleur@gmail.com>
2015-09-02 11:28:37 -0400

    Corrected error to help find controller

--- src/main/java/family/main/Application.java
+++ src/main/java/family/main/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@ComponentScan
+@ComponentScan(basePackages={"family.controller"})
 @Configuration
 @SpringBootApplication
 public class Application {
