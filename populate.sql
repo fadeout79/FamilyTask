73cb430864f54575b97b8686501e742c13091f8a
Ian-Remi Lafleur <irlafleur@gmail.com>
2015-09-01 20:07:30 -0400

    Added setup files and populate files for app testing

---
+++ populate.sql
@@ -0,0 +1,6 @@
+INSERT INTO person (name, dateOfBirth) VALUES ('Marilune', '2007-12-20');
+INSERT INTO person (name, dateOfBirth) VALUES ('Thiéry', '2009-09-20');
+INSERT INTO person (name, dateOfBirth) VALUES ('Maud', '1999-06-20');
+INSERT INTO person (name, dateOfBirth) VALUES ('Mégane', '2012-07-13');
+INSERT INTO person (name, dateOfBirth, isParent) VALUES ('Dominique', '1977-07-07', TRUE);
+INSERT INTO person (name, dateOfBirth, isParent) VALUES ('Ian', '1979-07-30', TRUE);
