f351d655c1055f642846504dcf2f8ab401bd582c
Ian-Remi Lafleur <irlafleur@gmail.com>
2015-09-12 11:00:48 -0400

    Continue implementation of tests

---
+++ tests/model/PersonTest.java
@@ -0,0 +1,22 @@
+package family.model;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import org.junit.Test;
+
+public class PersonTest {
+
+	//ApplicationContext ac;
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
