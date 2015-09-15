bccaceb9019c6dc00a8ce3cc341b1996ca81872d
Ian-Remi Lafleur <irlafleur@gmail.com>
2015-09-15 16:38:20 -0400

    Added methods to ImageMvcController to display and receive new files

--- src/main/java/family/controller/ImageMvcController.java
+++ src/main/java/family/controller/ImageMvcController.java
@@ -3,10 +3,17 @@ package family.controller;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
- 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,4 +25,58 @@ public class ImageMvcController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(ImageMvcController.class);
 
+    /**
+     * Upload single file using Spring Controller
+     */
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    public @ResponseBody
+    String uploadFileHandler(@RequestParam("name") String name,
+            @RequestParam("file") MultipartFile file) {
+ 
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+ 
+                // Creating the directory to store file
+                String rootPath = System.getProperty("catalina.home");
+                File dir = new File(rootPath + File.separator + "tmpFiles");
+                if (!dir.exists())
+                    dir.mkdirs();
+ 
+                // Create the file on server
+                File serverFile = new File(dir.getAbsolutePath()
+                        + File.separator + name);
+                BufferedOutputStream stream = new BufferedOutputStream(
+                        new FileOutputStream(serverFile));
+                stream.write(bytes);
+                stream.close();
+ 
+                logger.info("Server File Location="
+                        + serverFile.getAbsolutePath());
+ 
+                return "You successfully uploaded file=" + name;
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name
+                    + " because the file was empty.";
+        }
+    }
+    
+    @RequestMapping(value = "/getImage/{imageId}")
+    @ResponseBody
+    public byte[] getImage(@PathVariable long imageId, HttpServletRequest request)  {
+        byte[] data = null;
+    	try {
+		    String rpath=request.getRealPath("/");
+		    rpath=rpath+"/"+imageId; // whatever path you used for storing the file
+		    Path path = Paths.get(rpath);
+				data = Files.readAllBytes(path);
+    	} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	    return data;
+    }
 }
