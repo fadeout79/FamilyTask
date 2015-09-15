4eace71fc24bd33122e55abff21cd349a88cdb6f
Ian-Remi Lafleur <irlafleur@gmail.com>
2015-09-15 16:32:22 -0400

    Added methods to the file upload stuff

--- src/main/java/family/controller/ImageMvcController.java
+++ src/main/java/family/controller/ImageMvcController.java
@@ -1,5 +1,21 @@
 package family.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+ 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
 public class ImageMvcController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ImageMvcController.class);
 
 }
