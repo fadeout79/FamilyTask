5f5b5f4e38751d9dc18f429a7b3f17e53d76104e
Ian-Remi Lafleur <irlafleur@gmail.com>
2015-09-23 23:56:26 -0400

    Corrected indentation

--- src/main/java/utilities/ImageProcessing.java
+++ src/main/java/utilities/ImageProcessing.java
@@ -21,85 +21,71 @@ import family.controller.ImageMvcController;
 
 public class ImageProcessing {
 
-		public static final int IMAGE_WIDTH = 80;
-		public static final int IMAGE_HEIGHT = 80;
-		private String filePath;
-		
-		public String getFilePath() {
-			return filePath;
-		}
+	public static final int IMAGE_WIDTH = 80;
+	public static final int IMAGE_HEIGHT = 80;
+	private String filePath;
 
-		private void setFilePath(String fileName, String imagePath) {
-            String rootPath = imagePath;
-            File dir = new File(rootPath);
-            if (!dir.exists())
-                dir.mkdirs();
-            
-			this.filePath = dir.getAbsolutePath() + File.separator + fileName;;
-		}
+	public String getFilePath() {
+		return filePath;
+	}
+
+	private void setFilePath(String fileName, String imagePath) {
+		String rootPath = imagePath;
+		File dir = new File(rootPath);
+		if (!dir.exists())
+			dir.mkdirs();
+
+		this.filePath = dir.getAbsolutePath() + File.separator + fileName;;
+	}
 
 
-		private static final List<String> imageExtensions = Arrays.asList("jpg", "gif", "png");
-		
-		private static final Logger logger = LoggerFactory.getLogger(ImageProcessing.class);
+	private static final List<String> imageExtensions = Arrays.asList("jpg", "gif", "png");
 
-		public void createImage(MultipartFile file, String fileName, String imagePath) {
-			try {
-				if (isValidImage(fileName)) {
-					setFilePath(fileName, imagePath);
-					BufferedImage tempImage = convertMultipartToBufferedImage(file, fileName, imagePath);
-					BufferedImage resizedImage = new BufferedImage(IMAGE_WIDTH, IMAGE_HEIGHT, BufferedImage.TYPE_INT_RGB);
-					Graphics2D g = resizedImage.createGraphics();
-					g.drawImage(tempImage, 0, 0, IMAGE_WIDTH, IMAGE_HEIGHT, null);
-					g.dispose();		
-					
-						ImageIO.write(resizedImage, "jpg", new File(this.filePath));
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+	private static final Logger logger = LoggerFactory.getLogger(ImageProcessing.class);
+
+	public void createImage(MultipartFile file, String fileName, String imagePath) {
+		try {
+			if (isValidImage(fileName)) {
+				setFilePath(fileName, imagePath);
+				BufferedImage tempImage = convertMultipartToBufferedImage(file, fileName, imagePath);
+				BufferedImage resizedImage = new BufferedImage(IMAGE_WIDTH, IMAGE_HEIGHT, BufferedImage.TYPE_INT_RGB);
+				Graphics2D g = resizedImage.createGraphics();
+				g.drawImage(tempImage, 0, 0, IMAGE_WIDTH, IMAGE_HEIGHT, null);
+				g.dispose();		
+				logger.info("Writing file " + this.filePath);
+				ImageIO.write(resizedImage, "jpg", new File(this.filePath));
+			} else {
+				logger.info("Invalid filename : " + getExtension(fileName));
 			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-		private String getExtension(String fileName) {
-			return fileName.substring(fileName.lastIndexOf('.')).toLowerCase();
-		}
-		
-		private boolean isValidImage(String fileName) {
-			return imageExtensions.contains(getExtension(fileName));
-		}
-		
-		
-		private BufferedImage convertMultipartToBufferedImage(MultipartFile file, String fileName, String imagePath) {
-			BufferedImage bImageFromConvert = null;
-	        if (!file.isEmpty()) {
-	            try {
-	                byte[] bytes = file.getBytes();
-	                InputStream in = new ByteArrayInputStream(bytes);
-	    			bImageFromConvert = ImageIO.read(in);
+	}
 
-	    			
-//	                // Creating the directory to store file
-//	                String rootPath = imagePath;
-//	                File dir = new File(rootPath);
-//	                if (!dir.exists())
-//	                    dir.mkdirs();
-//	                fullPath = dir.getAbsolutePath() + File.separator + fileName;
-//	 
-//	                // Create the file on server
-//	                File serverFile = new File(fullPath);
-//	                BufferedOutputStream stream = new BufferedOutputStream(
-//	                        new FileOutputStream(serverFile));
-//	                stream.write(bytes);
-//	                stream.close();
-//	 
-//	                logger.info("Server File Location="
-//	                        + serverFile.getAbsolutePath());
-	            } catch (Exception e) {
-	                logger.error("You failed to upload " + fileName + " => " + e.getMessage());
-	            }
-	        } 
-	        return bImageFromConvert;
+	private String getExtension(String fileName) {
+		return fileName.substring(fileName.lastIndexOf('.')+1).toLowerCase();
+	}
+
+	private boolean isValidImage(String fileName) {
+		return imageExtensions.contains(getExtension(fileName));
+	}
+
+
+	private BufferedImage convertMultipartToBufferedImage(MultipartFile file, String fileName, String imagePath) {
+		BufferedImage bImageFromConvert = null;
+		if (!file.isEmpty()) {
+			try {
+				byte[] bytes = file.getBytes();
+				InputStream in = new ByteArrayInputStream(bytes);
+				bImageFromConvert = ImageIO.read(in);
+			} catch (Exception e) {
+				logger.error("You failed to upload " + fileName + " => " + e.getMessage());
+			}
+		} else {
+			logger.info("File is empty");
 		}
-		
+		return bImageFromConvert;
+	}
+
 }
